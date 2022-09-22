e599336d1d901555876f842db779aced7473d2d1
vijay <isreallit@gmail.com>
2022-09-22 15:46:07 +0530

    files

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+
+function myFunction(){
+    var x = document.getElementById("menu");
+        if(x.style.display == "block")
+        {
+          x.style.display = "none";
+        }
+        else{
+          x.style.display = "block";
+        }
+  }
+  
